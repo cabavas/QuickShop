bd57cc89f68fb20a711c42003235a0e2d385b309
Cabavas <cabavas@gmail.com>
2025-03-31 15:09:46 -0300

    project created

---
+++ src/test/java/tech/vascon/basketservice/BasketserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.vascon.basketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
